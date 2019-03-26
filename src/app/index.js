18d9acbec7f267101c855dfb757a519e1ce6f17d
Evaldas <evaldas.cerniauskas@gmail.com>
2019-03-26 22:01:03 +0200

    first commit

--- src/app/index.js
+++ src/app/index.js
@@ -1,6 +1,6 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./index.scss";
 
 class App extends Component {
   render() {
@@ -17,7 +17,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            EVALDAS
           </a>
         </header>
       </div>
